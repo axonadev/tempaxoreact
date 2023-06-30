9caa33ac3667630443e99c4d0197c0fb5dd8f4c8
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-06-30 15:47:52 +0200

    add Layout and ctrl token

--- template/src/App.js
+++ template/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { useToken } from "axonalib";
+import Layout from "./Layout/Layout";
+import { LoadingSpinner } from "axonaui";
 
 const App = () => {
-  return <div className="App"></div>;
+  const { connesso } = useToken(
+    process.env.REACT_APP_SERVERAPI + "api/axo_login",
+    localStorage.getItem("axn_token")
+  );
+
+  return (
+    <div className="App">
+      {connesso === 0 && <LoadingSpinner />}
+      {connesso === 1 && <Layout />}
+    </div>
+  );
 };
 
 export default App;
