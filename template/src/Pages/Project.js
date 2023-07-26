2c93cd3f57aea8f3164765e0971ecb7c9335f842
Emanuele 'Adekro' Croce <e.croce88@gmail.com>
2023-07-26 11:42:28 +0200

    add insert button

--- template/src/Pages/Project.js
+++ template/src/Pages/Project.js
@@ -7,16 +7,25 @@ const Project = () => {
   const cmd_getGrid = "/soggetti/soggettisel/leggisoggetti";
   const moduloForm = "soggetti";
 
+  const [focusForm, setFocusForm] = useState("");
+  const [statoGriglia, setStatoGriglia] = useState("");
   const [idobj_T, setIdobj_T] = useState(0);
-  const { onChangeSelected } = useForm();
+  const { onChangeSelected, onReset } = useForm();
+
+  const insertClickHandler = (idGriglia) => {
+    const idform = "form_" + idGriglia.split("_")[1];
+    onReset();
+    setFocusForm(idform);
+    setStatoGriglia("INSERIMENTO");
+    setIdobj_T(0);
+  };
 
   return (
     <>
-      <label>form progetto</label>
-
       <Frame label="TESTATA" type="form_t">
         <Grid
           id="maint_t"
+          stato={statoGriglia}
           loadGrid={
             process.env.REACT_APP_SERVERAPI +
             "api/axo_sel/" +
@@ -26,6 +35,8 @@ const Project = () => {
           nameView={nameView}
           onClickRow={(IDOBJ) => {
             setIdobj_T(IDOBJ);
+            setFocusForm("form_t");
+            setStatoGriglia("");
             onChangeSelected(
               process.env.REACT_APP_SERVERAPI +
                 "api/axo_sel/" +
@@ -39,32 +50,35 @@ const Project = () => {
           onDoubleClickRow={() => {
             console.log("click");
           }}
+          onBtnInsert={insertClickHandler}
         />
       </Frame>
-      <Form
-        id="form_t"
-        idobj={idobj_T}
-        modulo={moduloForm}
-        db={nameTable}
-        serverApi={process.env.REACT_APP_SERVERAPI}
-      >
-        <Frame label="DATI DI PROVA">
-          <FrameInRow width={[80, 10, 10]}>
-            <Input label="prova" id="Soggetti_Nome1"></Input>
-            <Input label="prova" id="Soggetti_Nome2"></Input>
-            <Input label="prova"></Input>
-          </FrameInRow>
-          <FrameInRow width={[30, 30, 40]}>
-            <Input label="prova"></Input>
-            <Input label="prova"></Input>
-            <Input label="prova"></Input>
-          </FrameInRow>
-          <FrameInRow width={[20, 20]}>
-            <Input label="prova"></Input>
-            <Input label="prova"></Input>
-          </FrameInRow>
-        </Frame>
-      </Form>
+      {focusForm === "form_t" && (
+        <Form
+          id="form_t"
+          idobj={idobj_T}
+          modulo={moduloForm}
+          db={nameTable}
+          serverApi={process.env.REACT_APP_SERVERAPI}
+        >
+          <Frame label="DATI DI PROVA">
+            <FrameInRow width={[80, 10, 10]}>
+              <Input label="prova" id="Soggetti_Nome1"></Input>
+              <Input label="prova" id="Soggetti_Nome2"></Input>
+              <Input label="prova"></Input>
+            </FrameInRow>
+            <FrameInRow width={[30, 30, 40]}>
+              <Input label="prova"></Input>
+              <Input label="prova"></Input>
+              <Input label="prova"></Input>
+            </FrameInRow>
+            <FrameInRow width={[20, 20]}>
+              <Input label="prova"></Input>
+              <Input label="prova"></Input>
+            </FrameInRow>
+          </Frame>
+        </Form>
+      )}
     </>
   );
 };
